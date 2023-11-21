f50c6605dfde4b4e6b276ce8699e979d5de8bfde
clocloduv6 <151666829+clocloduv6@users.noreply.github.com>
2023-11-21 13:09:26 -0500

    Update Thermodev#1.py

--- Thermodev#1.py
+++ Thermodev#1.py
@@ -9,17 +9,49 @@ import thermo as th
 import numpy as np
 import matplotlib.pyplot as plt
 
-#propylène glycole : 4254-14-2 a 30 % 
-#eau : 7732-18-5
+prop_gly = "4254-14-2"
+eau = "7732-18-5"
 
 T = -3.5 + 273.15 
-P = 101325
+P = 101325 #on pose l'hypothèse que l'on est à pression atmosphérique dans
+#la brasserie
 
-Mix = th.Mixture(('4254-14-2', '7732-18-5'), zs = (0.30,0.70), T=T, P=P)
-eau = th.Mixture(('4254-14-2', '7732-18-5'), zs = (0.0,0.100), T=T, P=P)
+#Mix = th.Mixture((prop_gly, eau), zs = (0.30,0.70), T=T, P=P)
+#eau = th.Mixture((prop_gly, eau), zs = (0.0,0.100), T=T, P=P)
 
-print('Le CP du mélange est de ', Mix.Cp, 'J/Kg * K ')
+temp=np.arange(-10,5,0.1)
+k_mel=[]
+mu_mel=[]
+Cp_mel=[]
 
-print('le k du mélange est de ', Mix.k, 'W/mK ')
+for i in range(len(temp)):
+  Mix = th.Mixture((prop_gly, eau), zs = (0.30,0.70), T=temp[i]+273.15, P=P)
+  k_mel.append(Mix.k)
+  mu_mel.append(Mix.mu)
+  Cp_mel.append(Mix.Cp)
 
-print('le H du mélange est de ', Mix.H, 'W/m^2K ')
+plt.figure()
+plt.plot(temp,k_mel,'g-o')
+plt.xlabel('Température (°C)')
+plt.grid(True)
+plt.ylabel("Conductivité thermique du mélange de propylène glycol et d'eau (W*K/m)")
+plt.title('Conductivité thermique du mélange de refroidissement entre -10°C et 5°C')
+plt.show()
+
+plt.figure()
+plt.plot(temp,mu_mel,'r-s')
+plt.xlabel('Température (°C)')
+plt.grid(True)
+plt.ylabel("Viscosité dynamique du mélange de propylène glycol et d'eau (Pa*s)")
+plt.title('Viscosité dynamique du mélange de refroidissement entre -10°C et 5°C')
+plt.show()
+
+plt.figure()
+plt.plot(temp,Cp_mel,'b-d')
+plt.xlabel('Température (°C)')
+plt.grid(True)
+plt.ylabel("Capacité thermique à pression constante du mélange de propylène glycol et d'eau (J*K/kg)")
+plt.title('Capacité thermique à pression constante du mélange de refroidissement entre -10°C et 5°C')
+plt.show()
+
+
