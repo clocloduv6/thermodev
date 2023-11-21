8499e8f1a2cc81663e06b87c178b323f46b53e1e
Megd172002 <151666943+Megd172002@users.noreply.github.com>
2023-11-21 12:48:00 -0500

    Update Thermodev#1.py

--- Thermodev#1.py
+++ Thermodev#1.py
@@ -9,3 +9,17 @@ import thermo as th
 import numpy as np
 import matplotlib.pyplot as plt
 
+#propylène glycole : 4254-14-2 a 30 % 
+#eau : 7732-18-5
+
+T = -3.5 + 273.15 
+P = 101325
+
+Mix = th.Mixture(('4254-14-2', '7732-18-5'), zs = (0.30,0.70), T=T, P=P)
+eau = th.Mixture(('4254-14-2', '7732-18-5'), zs = (0.0,0.100), T=T, P=P)
+
+print('Le CP du mélange est de ', Mix.Cp, 'J/Kg * K ')
+
+print('le k du mélange est de ', Mix.k, 'W/mK ')
+
+print('le H du mélange est de ', Mix.H, 'W/m^2K ')
